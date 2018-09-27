77c4d01e98368b5c0ddc86805ede0283fef8c685
Nerimene Akari <akari.nerimene@gmail.com>
2018-09-27 10:50:14 +0200

    passport

---
+++ config/passport/passport-setup.js
@@ -0,0 +1,24 @@
+const passport = require("passport");
+
+const User = require("../../models/User.js");
+
+// serializeUser(): defines what user data to save in the session
+// (happens when you log in successfully)
+passport.serializeUser((userDoc, done) => {
+  console.log("SERIALIZE (save user IDto session");
+
+  // "null" in the 1st argument tells Passport that there is "null" errors
+  done(null, userDoc._id);
+});
+
+// deserializeUser(): defines how to retrieve the user information from the DB
+// (happens automatically on EVERY request AFTER you log in)
+passport.deserializeUser((userId, done) => {
+  console.log("DESERIALIZE (retrive user info from the DB");
+  User.findById(userId)
+    .then(userDoc => {
+      // "null" in the 1st argument tells Passport that there is "null" errors
+      done(null, userDoc);
+    })
+    .catch(err => done(err));
+});
